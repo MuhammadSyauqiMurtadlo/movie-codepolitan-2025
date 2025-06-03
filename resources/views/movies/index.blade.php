6515b254a34e8e5988c54b2c397c42dc66662bd9
Syauqi <sauki084@gmail.com>
2025-06-04 04:22:14 +0700

    Fix card display

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,9 +6,8 @@
             <a href="{{ route('movie.show', $loop->index) }}">
                 <div class="bg-gray-800 p-4 rounded-lg shadow-lg text-center relative group">
                     <img src="{{ $movie['image'] }}" alt="Movies" class="w-full rounded-medium">
-                    <h3 class="text-lg mt-2 font-bold mb-4">{{ $movie['title'] }}</h3>
+                    <h3 class="text-md mt-2 font-md mb-4 line-clamp-2 h-14">{{ $movie['title'] }}</h3>
                     <p class="text-sm text-gray-400">{{ $movie['release_date'] }}</p>
-
                     <div class="absolute top-2 right-2 space-x-2 opacity-0 group-hover:opacity-100 transition">
                         <button class="bg-green-600 p-1 rounded hover:bg-green-500">✏️</button>
                         <button class="bg-red-600 p-1 rounded hover:bg-red-500">🗑️</button>
