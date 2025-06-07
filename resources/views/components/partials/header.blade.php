ae099178c5681c5efdcb9d596e7ad3cf42925c4f
Syauqi <sauki084@gmail.com>
2025-06-07 16:33:40 +0700

    Fix error components anonymous

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="flex justify-between p-5 bg-gray-800">
     <a href="{{ route('movie.index') }}" class="text-white">
-        <h1 class="text-2xl font-bold">Movie App</h1>
+        <h1 class="text-2xl font-bold">Movie</h1>
     </a>
     <a href="{{ route('movie.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
         Add Movie
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 <x-app>
-    <div class="bg-black text-white p-8 rounded-lg shadow-lg max-w-md mx-auto mt-20">
+    <div class="bg-black text-white p-8 rounded-lg shadow-lg mt-5 max-w-md mx-auto text-center">
         <h1 class="text-3xl font-bold mb-4">Welcome to Our Application</h1>
         <p class="text-gray-700 mb-6">This is a simple welcome page built with Laravel and Tailwind CSS.</p>
         <a href="{{ route('login') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Login</a>
-        <a href="{{ route('register') }}"
-            class="ml-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Register</a>
+        {{-- <a href="{{ route('register') }}"
+            class="ml-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Register</a> --}}
     </div>
 </x-app>
