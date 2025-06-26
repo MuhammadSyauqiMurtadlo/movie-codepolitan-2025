6c34a815f66797c371b06101c9363a3f476c18ae
Syauqi <sauki084@gmail.com>
2025-06-26 21:21:06 +0700

    Update button header

--- resources/views/_partials/_header.blade.php
+++ resources/views/_partials/_header.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('movie.index') }}" class="text-white">
         <h1 class="text-2xl font-bold">Movie App</h1>
     </a>
-    <a href="{{ route('movie.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+    <a href="{{ route('movie.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
         Add Movie
     </a>
 </header>
